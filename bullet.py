c75d865152b42fdb66b43afc9ec6fc49220384dd
mbuechmann <malte.buechmann@gmail.com>
2013-10-06 18:20:49 +0200

    Correct collision detection of bullets and simplfy code

--- bullet.py
+++ bullet.py
@@ -15,15 +15,15 @@ class Bullet(GameObject):
         self.last_position = position
         self.vx = math.sin(rotation / 180 * math.pi) * self.SPEED
         self.vy = -math.cos(rotation / 180 * math.pi) * self.SPEED
+        self.speed = (self.vx, self.vy)
         self.ttl = self.TTL
 
     def render(self, screen):
         pygame.draw.rect(screen, (255, 255, 255), (self.position[0] - 1, self.position[1] - 1, 2, 2))
 
     def logic(self, delta):
-        self.last_position = self.position
-        self.speed = (delta * self.vx, delta * self.vy)
-        self.position = numpy.add(self.position, self.speed)
+        delta_speed = numpy.multiply(self.speed, delta)
+        self.last_position = numpy.subtract(self.position, delta_speed)
         self.move(delta)
         self.ttl -= delta
 
